5c0e64a0c3eba0866da3f49d5089f5fbec46c25e
Anas Elkadiri <aelkadir@e2r6p9.1337.ma>
2025-01-26 15:48:28 +0100

    HH

--- fractol.c
+++ fractol.c
@@ -24,7 +24,7 @@ int	main(int argc, char **argv)
 		{
 			if (!is_valid(argv[2]) || !is_valid(argv[3]))
 			{
-				ft_putstr_fd("wrong params, try julia c.i c.i", 1);
+				ft_putstr_fd("wrong params, try julia c.r c.i", 1);
 				return (0);
 			}
 			fractal.julia_x = ft_atodouble(argv[2]);
